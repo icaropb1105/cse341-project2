faf3bb16251974bf8f3b7d927d5f166507341b2d
Icaro Angelo <angelopb1105@gmail.com>
2026-02-03 20:17:52 -0300

    fixing auth

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,10 +4,13 @@ const productController = require('../controllers/productController');
 const { validateProduct } = require('../middleware/validate');
 const auth = require('../middleware/auth');
 
+// Rotas públicas 
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.post('/', validateProduct, productController.createProduct);
-router.put('/:id', validateProduct, productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+
+// Rotas protegidas
+router.post('/', auth, validateProduct, productController.createProduct);
+router.put('/:id', auth, validateProduct, productController.updateProduct);
+router.delete('/:id', auth, productController.deleteProduct);
 
 module.exports = router;
